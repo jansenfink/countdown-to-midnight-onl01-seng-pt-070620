fe1026a6d247491515568fdcf52e07364f0fe914
Jansen Fink <jansenfink@charter.net>
2020-07-13 22:45:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,6 @@
 def countdown(number)
 
-  while number >= 0 
+  while number > 0 
     puts "#{number} SECOND(S)!"
     number -= 1
   end
@@ -9,7 +9,7 @@ def countdown(number)
 end
 
 def countdown_with_sleep(number)
-  while number >= 0 
+  while number > 0 
     puts "#{number} SECOND(S)!"
     number -= 1
     sleep 1
