abb70defa4a3bf53e11c9a8fb9769e827585becb
Jansen Fink <jansenfink@charter.net>
2020-07-13 22:39:10 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,13 @@
 number = 12
 def countdown(number)
- {{
+
   while number >= 0 
   puts "#{number} SECOND(S)!"
   number-=
-  }
-return "HAPPY NEW YEAR!" }
+end
+
+return "HAPPY NEW YEAR!" 
+end
 
 def countdown_with_sleep(number)
   while number >= 0 
